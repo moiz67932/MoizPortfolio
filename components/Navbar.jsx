fdc3611d04be4eeb07bfd5090434f170b9378d1d
Muhammad Moiz <moiz67932@gmail.com>
2024-07-15 13:18:55 +0500

    Calendly Integrated

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -60,8 +60,8 @@ const Navbar = () => {
             <Image
               src={NavLogo}
               alt='/'
-              width='125'
-              height='50'
+              width='60'
+              height='60'
               className='cursor-pointer'
             />
           </a>
